d0e835eb9031b98f7328f792ebf2d4d111c7f6d2
Emil Abbasov <emilabb13@gmail.com>
2025-11-10 21:53:32 +0400

    working on api configuration

--- api/api.rb
+++ api/api.rb
@@ -1,12 +1,27 @@
+require "faraday"
+require "json"
 require "dotenv/load"
 
 
 class CryptonAPI
   def initialize
-    @key = ENV["API_KEY"]
+    @conn = Faraday.new(
+      url: ENV["API_URL"],
+      headers: {
+        "Content-Type" => "application/json",
+        "Authorization" => "Bearer #{ENV["API_KEY"]}"
+      }
+    )
   end
 
   def get_data(symbol)
-    puts "Symbol: #{symbol}"
+    response = @conn.get("getData", { symbol: symbol })
+
+    if response.success?
+      data = JSON.pretty_generate(JSON.parse(response.body))
+      return data
+    else
+      puts "[ERROR]: There was problem about fetching the data..."
+    end
   end
 end
